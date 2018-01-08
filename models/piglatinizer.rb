e8a7ec812e7f89c682df4bfcb92e6371563c1203
Jered Sweeney <sweeneyjered@gmail.com>
2018-01-08 04:15:21 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -11,7 +11,7 @@ class PigLatinizer
       @pig = "#{word = "way"}"
     else
       index = word_array.find_index{|l| vowels.include?(l)}.to_i #index of first vowel
-      @pig = "#{word_array.drop(index).join + word[0..(index - 1] + "ay"}"
+      @pig = "#{word_array.drop(index).join + word[0..index - 1] + "ay"}"
       # @pig = "#{word.split("").drop(1).join + word[0] + "ay"}"
     end
   end

0d764a8ed4aa58f2bb9ae29c45da30fc8c4aee75
Jered Sweeney <sweeneyjered@gmail.com>
2018-01-08 04:09:42 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -10,7 +10,7 @@ class PigLatinizer
     elsif vowels.include?(word[0])
       @pig = "#{word = "way"}"
     else
-      index = word.split("").find_index{|l| vowels.include?(l)}.to_i #index of first vowel
+      index = word_array.find_index{|l| vowels.include?(l)}.to_i #index of first vowel
       @pig = "#{word.split("").find_index{|l| vowels.include?(l)}}"
       # @pig = "#{word.split("").drop(1).join + word[0] + "ay"}"
     end
